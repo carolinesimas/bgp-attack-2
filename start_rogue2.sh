2a4834f76cc369546dc61a2e521c4cd92ff140d1
carolinesimas <carol.simas92@gmail.com>
2017-12-12 10:08:47 -0300

    Delete start_rogue2.sh

--- start_rogue2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Killing any existing rogue AS"
-./stop_rogue.sh
-
-echo "Starting rogue AS"
-sudo python run.py --node R10 --cmd "/usr/lib/quagga/zebra -f conf/zebra-R10.conf -d -i /tmp/zebra-R10.pid > logs/R10-zebra-stdout"
-sudo python run.py --node R10 --cmd "/usr/lib/quagga/bgpd -f conf/bgpd-R10.conf -d -i /tmp/bgpd-R10.pid > logs/R10-bgpd-stdout"
